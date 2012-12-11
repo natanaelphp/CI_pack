c819a0b0911fb770c1e85f801e675766ce20b520
Natanael <natanael.php@gmail.com>
2012-12-11 15:54:59 -0200

    Primeiro commit

---
+++ application/modules/home/controllers/home.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+    
+    public function index()
+	{
+		$this->load->helper(array('url','html'));
+		$this->load->view('home');
+		//$this->load->view('marketing');
+		
+	}
+        
+}
+
+?>
